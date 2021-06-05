6bab6a7853e014002624d38f376003bd50ebb425
Alejandro <alejandroeguiazabal@gmail.com>
2021-06-05 12:06:29 -0300

    completo!

--- Complementarios2/Complementario6/src/com/alejandroeguiazabal/Main.java
+++ Complementarios2/Complementario6/src/com/alejandroeguiazabal/Main.java
@@ -3,6 +3,7 @@ package com.alejandroeguiazabal;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
 /* 6.	Se dispone de una lista de Empleados, de cada empleado se conoce:
@@ -15,17 +16,22 @@ Todos los empleados están cargados en un Set (HashSet), se desea calcular el su
 donde la clave (key) es el dni y el valor (value) es el sueldo calculado. */
 
     public static void main(String[] args) {
+        HashMap<Integer, Integer> Diccionario; //Definiendo el tipo de dato que va a contener la variable diccionario (def de la variable)
+        Diccionario = new HashMap<>(); //investigar los tipos de datos
 
-        HashSet<String> empleado = new HashSet<String>();
+        Set<Empleado> lista ; //definimos lista como una  collection Set que contiene empleados
+        lista = new HashSet<>(); // al ser set una interfase, la instanciamos como HasSet
+        lista.add(new Empleado("Lola", "Mento", 12457845, 8, 340));
+        //instanciar un nuevo objeto empleado, al cual le asigne los valores a sus propiedades, para agregar a la colection lista.
+        lista.add(new Empleado("Luz", "Roja", 98653298, 4, 332));
+        lista.add(new Empleado("Armando Esteban", "Quito", 54876532, 5, 310));
+        lista.add(new Empleado("Paca", "Garte", 56238945, 6, 456));
+        lista.add(new Empleado("Elena", "Nito", 32216554, 8, 440));
 
-        empleado.add("Paloma");
-        empleado.add("Almendra");
-        empleado.add("Camilo");
-        empleado.add("Alejandro");
-        empleado.add("Cecilia");
-
-        HashMap<String, Integer, Integer, Integer, Integer> calculoSueldo = new HashMap<>(0);
-        Map
-
+        for (Empleado emp : lista) {
+            int sueldo = Empleado.calcularSueldo(emp.horasTrabajadas, emp.valoPorHora);
+            Diccionario.put(emp.dni, sueldo);
+        }
+        System.out.print(Diccionario);
     }
 }
--- Complementarios2/Complementario7/src/com/alejandroeguiazabal/Main.java
+++ Complementarios2/Complementario7/src/com/alejandroeguiazabal/Main.java
@@ -1,48 +1,41 @@
 package com.alejandroeguiazabal;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-/*
-7.	Crear una función que dado 2 argumentos (int, siendo el primero menor al segundo), nos devuelva
-un array de Strings. Con la secuencia de números enteros de principio a final. Pero si el número es
-multiplo de 2 colocara el valor “Fizz”, si es múltiplo de 3 “Buzz” y si es a la vez múltiplo de ambos
- colocara “FizzBuzz”.
-Observacion: Los 2 argumentos indican con que valor se arranca a calcular y el segundo con qué valor
-debe frenar (no se incluye en el cálculo)
-Ejemplo: (1, 5) ----> calculará valores de 1, 2, 3, 4
+    /*
+    7.	Crear una función que dado 2 argumentos (int, siendo el primero menor al segundo), nos devuelva
+    un array de Strings. Con la secuencia de números enteros de principio a final. Pero si el número es
+    multiplo de 2 colocara el valor “Fizz”, si es múltiplo de 3 “Buzz” y si es a la vez múltiplo de ambos
+     colocara “FizzBuzz”.
+    Observacion: Los 2 argumentos indican con que valor se arranca a calcular y el segundo con qué valor
+    debe frenar (no se incluye en el cálculo)
+    Ejemplo: (1, 5) ----> calculará valores de 1, 2, 3, 4
 
- */
+     */
     public static void main(String[] args) {
 
         System.out.println("Ingrese dos números enteros (el primero menor que el segundo): ");
         Scanner scan = new Scanner(System.in);
         int numero1 = scan.nextInt();
         int numero2 = scan.nextInt();
-        System.out.print(frase(numero1, numero2));
         scan.close();
-    }
 
-    public static List frase(int num1, int num2) {
+        ArrayList<String> lista = new ArrayList<String>();
 
-        List numeros = new List();
-        for(int i = num1;i < num2; i++) {
-            if((i % 2 == 0) && (i % 3 == 0)) {
-                numeros.add("FizzBuzz");
-            }
-            else if(i % 2 == 0) {
-                numeros.add("Fizz");
-            }
-            else if(i % 3 == 0) {
-                numeros.add("Buzz");
-            }
-            else {
-                String b = String.valueOf(i);
-                numeros.add(b);
+        for (int i = numero1; i < numero2; i++) {
+            if ((i % 2 == 0) && (i % 3 == 0)) {
+                lista.add("FizzBuzz");
+            } else if (i % 2 == 0) {
+                lista.add("Fizz");
+            } else if (i % 3 == 0) {
+                lista.add("Buzz");
+            } else {
+                //int b = String.valueOf(i);
+                lista.add(String.valueOf(i));
             }
         }
-        return numeros;
+        System.out.println(lista);
     }
 }
